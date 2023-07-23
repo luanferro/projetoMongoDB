0b20bebcce09c807df4ac74f0b9379224c7db2d4
luan.ferro <luan.ferro@sensedia.com>
2023-07-23 20:57:40 -0300

    NotFound exception handling

--- src/main/java/com/luanferro/projetoMongoDB/services/UserService.java
+++ src/main/java/com/luanferro/projetoMongoDB/services/UserService.java
@@ -1,12 +1,14 @@
 package com.luanferro.projetoMongoDB.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luanferro.projetoMongoDB.domain.entities.User;
 import com.luanferro.projetoMongoDB.repositories.UserRepository;
+import com.luanferro.projetoMongoDB.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = repository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
     
 }
