30aed3fb67478e30f21866cbad8ccf317d0a8790
luan.ferro <luan.ferro@sensedia.com>
2023-07-23 21:12:06 -0300

    insert User

--- src/main/java/com/luanferro/projetoMongoDB/resources/UserResource.java
+++ src/main/java/com/luanferro/projetoMongoDB/resources/UserResource.java
@@ -1,17 +1,18 @@
 package com.luanferro.projetoMongoDB.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luanferro.projetoMongoDB.domain.dto.UserDTO;
 import com.luanferro.projetoMongoDB.domain.entities.User;
@@ -35,5 +36,13 @@ public class UserResource {
     public ResponseEntity<UserDTO> findAll(@PathVariable String id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody UserDTO userDTO) {
+        User user = service.fromDTO(userDTO);
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
--- src/main/java/com/luanferro/projetoMongoDB/services/UserService.java
+++ src/main/java/com/luanferro/projetoMongoDB/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.luanferro.projetoMongoDB.domain.dto.UserDTO;
 import com.luanferro.projetoMongoDB.domain.entities.User;
 import com.luanferro.projetoMongoDB.repositories.UserRepository;
 import com.luanferro.projetoMongoDB.services.exceptions.ObjectNotFoundException;
@@ -25,4 +26,11 @@ public class UserService {
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
     
+    public User insert(User user) {
+        return repository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+    }
 }
