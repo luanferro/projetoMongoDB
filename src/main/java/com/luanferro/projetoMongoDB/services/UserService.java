6bd66fdf4af9ff15b0a8cfba9858aa6daf3c8331
luan.ferro <luan.ferro@sensedia.com>
2023-07-23 23:56:46 -0300

    update User

--- src/main/java/com/luanferro/projetoMongoDB/services/UserService.java
+++ src/main/java/com/luanferro/projetoMongoDB/services/UserService.java
@@ -38,4 +38,15 @@ public class UserService {
         findById(id);
         repository.deleteById(id);
     }
+
+    public User update(User user) {
+        User newUser = findById(user.getId());
+        updateData(newUser, user);
+        return repository.save(newUser);
+    }
+
+    private void updateData(User newUser, User user) {
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+    }
 }
