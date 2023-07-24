58888347a08748c39fe688ee6da240d030e4b73a
luan.ferro <luan.ferro@sensedia.com>
2023-07-24 00:40:41 -0300

    Post reference in UserEntity

--- src/main/java/com/luanferro/projetoMongoDB/config/Instantiation.java
+++ src/main/java/com/luanferro/projetoMongoDB/config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.luanferro.projetoMongoDB.domain.dto.AuthorDTO;
 import com.luanferro.projetoMongoDB.domain.entities.Post;
 import com.luanferro.projetoMongoDB.domain.entities.User;
 import com.luanferro.projetoMongoDB.repositories.PostRepository;
@@ -35,11 +36,15 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viahar pra São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-        
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viahar pra São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+        
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 
 }
